1f5e7f0cd150fa2187c4f23baa95181818e495b4
zixu067-ship-it <zixu067-ship-it@users.noreply.github.com>
2026-05-20 06:05:48 +0000

    fix: rewrite invite page

--- web/classic/src/pages/Agent/Invite.jsx
+++ web/classic/src/pages/Agent/Invite.jsx
@@ -71,32 +71,22 @@ import React, { useEffect, useState } from 'react';
             <Title heading={2} style={{ color: '#fff', margin: 0 }}>邀请你加入代理小组</Title>
           </Space>
         </Card>
-
         <Card>
           <Space vertical align='center' style={{ width: '100%' }}>
             {info.avatar_url && <Avatar src={info.avatar_url} size='extra-large' />}
             <Title heading={3} style={{ margin: 0 }}>{info.group_name}</Title>
             {info.slogan && <Text type='tertiary'>{info.slogan}</Text>}
-
             <Tag size='large' color='red' style={{ fontSize: 16, padding: '8px 20px', marginTop: 12 }}>
               你将获得：{info.share_pct_in_group}% 分润占比
             </Tag>
-
             {info.message_to_members && (
               <Card style={{ background: '#f5f5f5', width: '100%', marginTop: 12 }}>
                 <Text type='tertiary'>组长寄语：</Text>
                 <div style={{ marginTop: 8 }}>{info.message_to_members}</div>
               </Card>
             )}
-
-            <Banner
-            <Banner
-              type='info'
-              icon={null}
-              description='接受邀请前，需要先完成代理资料填写。'
-              style={{ width: '100%', marginTop: 12 }}
-            />
-
+            <Banner type='info' icon={null} description='接受邀请前，需要先完成代理资料填写。' style={{ width: '100%',
+  marginTop: 12 }} />
             <Space style={{ marginTop: 16 }}>
               <Button onClick={() => navigate('/')}>暂不加入</Button>
               <Button theme='solid' type='primary' size='large' loading={accepting} onClick={handleAccept}>
